646d00a98165c2faa2923313b931f5db6848f3ed
Jonathan Diehl <jonathan.diehl@gmail.com>
2023-03-07 09:22:08 +0100

    Added import filter

--- plugin/index.js
+++ plugin/index.js
@@ -1,8 +1,16 @@
+function getImports(node) {
+  return node.specifiers
+    .filter(s => s.type === 'ImportSpecifier')
+    .map(s => s.imported.name)
+}
+
 module.exports = () => ({
   visitor: {
     ImportDeclaration: function (path, state) {
       if (path.node.source.value === 'react/jsx-runtime') {
-        path.node.source.value = '@dynatrace/jsx-runtime'
+        const imports = getImports(path.node)
+        // if (imports.length > 0 || imports[0] !== 'jsx') return
+        path.node.source.value = '@dynatrace/react-native-plugin/jsx-runtime'
       }
   },
   },
